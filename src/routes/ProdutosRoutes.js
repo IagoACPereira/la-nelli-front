92e9550d0a7c76a045051069f35b4f3a5c2f3dc3
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-08-26 06:52:13 -0300

    feat: :sparkles: add: Formulário para atualizar

--- src/routes/ProdutosRoutes.js
+++ src/routes/ProdutosRoutes.js
@@ -35,4 +35,31 @@ export default Router()
     } catch (error) {
       res.send(error.message);
     }
+  })
+  .get('/atualizar/produto', async (req, res) => {
+    const { token } = req.cookies;
+    const { idProduto } = req.query;
+    try {
+      const categorias = await axios.get(`${process.env.URLAPI}/categorias-produtos?limite=100000`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      const produto = await axios.get(`${process.env.URLAPI}/produtos/${idProduto}`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      res.render('produtos/atualizar-produto.ejs', {
+        categorias: categorias.data,
+        produto: produto.data,
+        urlApi: process.env.URLAPI,
+        token,
+        idProduto,
+      });
+    } catch (error) {
+      res.send(error.message);
+    }
   });
